5ee6c662fb29696622ba27469512c0a8a6f50ab0
AlexBof <o-batsev@online.no>
2015-10-27 19:16:19 +0100

    main comp

--- TestApp1/CL/fft256.cl
+++ TestApp1/CL/fft256.cl
@@ -1,4 +1,78 @@
-kernel void fft256(global read_only float2* in, global write_only float2* out) 
+#define n 4
+#define N 256
+#define N0 256
+#define N1 64
+#define N2 16
+#define N3 4
+#define N_4_0 0u
+#define N_4_1 64u
+#define N_4_2 64u * 2
+#define N_4_3 64u * 3
+#define N_3_0 0u
+#define N_3_1 16u
+#define N_3_2 16u * 2
+#define N_3_3 16u * 3
+
+__attribute__((always_inline))
+float2 iMult(float2 x)
 {
+	return (float2)(-x.y, x.x);
+}
+
+__attribute__((always_inline))
+float2 twiddle(uint ik, uint N)
+{
+	float angle = (M_PI * 2.0f * ik) / N;
+	return (float2)(native_cos(angle), native_sin(angle));
+
+}
+
+kernel void fft256(global read_only float2 in[N], global write_only float2 out[N]) 
+{
+	size_t j = get_local_id(0);
+	float2 x0 = in[j + N_4_0];
+	float2 x2 = in[j + N_4_2];
+	float2 x1 = in[j + N_4_1];
+	float2 x3 = in[j + N_4_3];
+
+	float2 a0 = x0 + x2;
+	float2 b0 = x0 - x2;
+	float2 a1 = x1 + x3;
+	float2 b1 = iMult(x1 - x3);
+
+	x0 = (a0 + a1) * twiddle(j * 0, N0);
+	x1 = (b0 + b1) * twiddle(j * 1, N0);
+	x2 = (a0 - a1) * twiddle(j * 2, N0);
+	x3 = (b0 - b1) * twiddle(j * 3, N0);
+
+	local float2 loc[N_4_1 * 5];
+	local float2* loc_in = loc + (j >> 2) * 5 + j & 3u;
+
+	uint indOut = j;
+	uint stride = 0u;
+	local float2* loc_out = 
+	loc[indOut * 5 + 0] = z0;
+	loc[indOut * 5 + 1] = z1;
+	loc[indOut * 5 + 2] = z1;
+	loc[indOut * 5 + 3] = z1;
+
+	//////////    stage 1 ////////////
+	j >>= 2;
+	x0 = *(loc_in + N_3_0 * 5);
+	x1 = *(loc_in + N_3_1 * 5);
+	x2 = *(loc_in + N_3_2 * 5);
+	x3 = *(loc_in + N_3_3 * 5);
+
+	float2 a0 = x0 + x2;
+	float2 b0 = x0 - x2;
+	float2 a1 = x1 + x3;
+	float2 b1 = iMult(x1 - x3);
+
+	x0 = (a0 + a1) * twiddle(j * 0, N1);
+	x1 = (b0 + b1) * twiddle(j * 1, N1);
+	x2 = (a0 - a1) * twiddle(j * 2, N1);
+	x3 = (b0 - b1) * twiddle(j * 3, N1);
+	
+	indOut = 
 
 }
--- TestApp1/Program.cs
+++ TestApp1/Program.cs
@@ -6,51 +6,68 @@ using Cloo;
 
 namespace TestApp1
 {
-    static class Program
+    public class GpuProgram
     {
-        static void Main(string[] args)
+        public readonly ComputeContext Context;
+        public readonly ComputeDevice Device;
+        public readonly ComputeKernel Kernel;
+        private readonly ComputeProgram Program;
+        public readonly ComputeCommandQueue Queue;
+
+        public GpuProgram(string file, string kernelName)
         {
-            var device =
+            Device =
                 ComputePlatform.Platforms.SelectMany(p => p.Devices)
                     .FirstOrDefault(d => d.Type == ComputeDeviceTypes.Gpu);
-            if(device == null)
+            if (Device == null)
                 return;
-            var context = new ComputeContext(new List<ComputeDevice> {device},
-                new ComputeContextPropertyList(device.Platform), null, IntPtr.Zero);
-            var queue = new ComputeCommandQueue(context, device, ComputeCommandQueueFlags.None);            
-            var program = new ComputeProgram(context, new StreamReader(@"CL\kernel1.cl").ReadToEnd());
+            Context = new ComputeContext(new List<ComputeDevice> {Device},
+                new ComputeContextPropertyList(Device.Platform), null, IntPtr.Zero);
+            Queue = new ComputeCommandQueue(Context, Device, ComputeCommandQueueFlags.None);
+            Program = new ComputeProgram(Context, new StreamReader(@"CL\" + file).ReadToEnd());
             try
             {
-                program.Build(null, null, null, IntPtr.Zero);
+                Program.Build(null, null, null, IntPtr.Zero);
             }
             catch (Exception)
             {
-
-                throw new Exception(program.GetBuildLog(device));
+                throw new Exception(Program.GetBuildLog(Device));
             }
-            var kernel = program.CreateKernel("helloWorld");
+            Kernel = Program.CreateKernel(kernelName);
+        }
+    }
 
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            TestNativeCos();
+        }
+
+        private static void TestNativeCos()
+        {
+            var gpu = new GpuProgram("kernel1.cl", "helloWorld");
 
             const int n = 100;
             var fullCos = new float[n];
             var nativeCos = new float[n];
             var doubleCos = Enumerable.Range(0, n).Select(i => Math.Cos(Math.PI * 2.0 / n * i)).ToArray();
-            var fullCosBuffer = new ComputeBuffer<float>(context,
+            var fullCosBuffer = new ComputeBuffer<float>(gpu.Context,
                 ComputeMemoryFlags.WriteOnly | ComputeMemoryFlags.UseHostPointer, fullCos);
-            var nativeCosBuffer = new ComputeBuffer<float>(context,
+            var nativeCosBuffer = new ComputeBuffer<float>(gpu.Context,
                 ComputeMemoryFlags.WriteOnly | ComputeMemoryFlags.UseHostPointer, nativeCos);
 
-            kernel.SetMemoryArgument(0, fullCosBuffer); // set the integer array
-            kernel.SetMemoryArgument(1, nativeCosBuffer); // set the integer array
+            gpu.Kernel.SetMemoryArgument(0, fullCosBuffer); // set the integer array
+            gpu.Kernel.SetMemoryArgument(1, nativeCosBuffer); // set the integer array
 
-            queue.Execute(kernel, null, new long[] {n}, new long[] {n}, null);
-            queue.ReadFromBuffer(fullCosBuffer, ref fullCos, true, null);
-            queue.ReadFromBuffer(nativeCosBuffer, ref nativeCos, true, null);
+            gpu.Queue.Execute(gpu.Kernel, null, new long[] {n}, new long[] {n}, null);
+            gpu.Queue.ReadFromBuffer(fullCosBuffer, ref fullCos, true, null);
+            gpu.Queue.ReadFromBuffer(nativeCosBuffer, ref nativeCos, true, null);
             // wait for completion
 
             var diffs = fullCos.Zip(nativeCos, (f, f1) => f1 != 0.0 ? Math.Abs((f - f1) / f1) : 0.0f).ToArray();
             var maxDiff = diffs.Max();
-            queue.Finish();
+            gpu.Queue.Finish();
         }
     }
 }
